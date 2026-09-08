0155539e0499014e39fd07be8896b61dea71aa73
VIGNESH RAGAVI.K <vigneshragavikannan@gmail.com>
2026-09-08 20:43:17 +0530

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: '/casaterminal/',
+
   plugins: [react()],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
