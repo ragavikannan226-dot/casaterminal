da5822585b8ebadaa05d0096e5bd366fb06f51a1
VIGNESH RAGAVI.K <vigneshragavikannan@gmail.com>
2026-09-17 17:43:10 +0530

    Fix GitHub Pages asset path

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
 
-  base: '/casa-terminal/',
+  // GitHub repository name
+  base: "/casaterminal/",
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 
   build: {
-    outDir: 'dist',
+    outDir: "dist",
   },
 });
